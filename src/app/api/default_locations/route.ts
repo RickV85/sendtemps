00bb1cdbe4c00632b6eb518f617252d53cace5fc
Rick Vermeil <rickv85@gmail.com>
2023-12-05 15:23:30 -0700

    Change caching policy of default locations GET to 10 min max age

--- src/app/api/default_locations/route.ts
+++ src/app/api/default_locations/route.ts
@@ -7,9 +7,10 @@ export async function GET() {
       await sql`SELECT * FROM weather_wise.default_locations;`;
     const defaultLocRows = defaultLocations.rows;
     const response = NextResponse.json(defaultLocRows, { status: 200 });
-    // Added no-cache for build phase, change to "Cache-Control", "public, max-age=3600"
-    // or similar to cache request
-    response.headers.set("Cache-Control", "no-cache");
+    // Added cache max age of 10 min for build phase, 
+    // change to "Cache-Control", "public, max-age=3600"
+    // for one hour, or maybe more for production
+    response.headers.set("Cache-Control", "public, max-age=600");
     return response;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
