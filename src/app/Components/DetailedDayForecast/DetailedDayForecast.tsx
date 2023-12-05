02a8e1a6387f35286488708f00d5648c419c2e53
Rick Vermeil <rickv85@gmail.com>
2023-12-04 18:14:44 -0700

    Add relative humidity and weather icon img to daily forecasts

--- src/app/Components/DetailedDayForecast/DetailedDayForecast.tsx
+++ src/app/Components/DetailedDayForecast/DetailedDayForecast.tsx
@@ -1,16 +1,32 @@
-
+import Image from "next/image";
 interface Props {
   data: {
     name: string;
     detailedForecast: string;
+    relativeHumidity: {
+      value: number;
+    };
+    icon: string;
   };
 }
 
 const DetailedDayForecast: React.FC<Props> = ({ data }) => {
   return (
     <article className="detailed-day-forecast">
-      <h3>{data.name}</h3>
-      <p>{data.detailedForecast}</p>
+        <h3>{data.name}</h3>
+      <div className="day-forecast-header">
+        <Image
+          src={data.icon}
+          height={60}
+          width={60}
+          alt="weather icon"
+          className="day-forecast-icon"
+        />
+      <p className="day-forecast-text">
+        {data.detailedForecast} Relative humidity: {data.relativeHumidity.value}
+        %
+      </p>
+      </div>
     </article>
   );
 };
