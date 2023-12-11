ab390418c477fe8b8c41679cda3954d1da9b0e0d
Rick Vermeil <rickv85@gmail.com>
2023-12-11 14:29:43 -0700

    Create initial users GET endpoint for querying user by user_id search param

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,8 +1,23 @@
 import { sql } from "@vercel/postgres";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const userId = request.nextUrl.searchParams.get("user_id");
   try {
-    
+    const { rows } = await sql`SELECT * FROM sendtemps.users 
+    WHERE id = ${userId};`;
+    if (rows.length > 1) {
+      throw new Error (`More than one user found for id: ${userId}`)
+    }
+    const foundUser = rows[0];
+    let response;
+    if (foundUser) {
+      response = NextResponse.json(foundUser, { status: 200 });
+    } else {
+      response = NextResponse.json(`No user found with id: ${userId}`, { status: 200 });
+    }
+    return response;
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
