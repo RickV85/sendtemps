ed17616919ac8abcbcd86bc3603d114bd60b01fd
Rick Vermeil <rickv85@gmail.com>
2023-12-11 11:53:23 -0700

    Change schema name to sendtemps after change in DB to reflect new app name

--- src/app/api/default_locations/route.ts
+++ src/app/api/default_locations/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const defaultLocations =
-      await sql`SELECT * FROM weather_wise.default_locations;`;
+      await sql`SELECT * FROM sendtemps.default_locations;`;
     const defaultLocRows = defaultLocations.rows;
     const response = NextResponse.json(defaultLocRows, { status: 200 });
     return response;
