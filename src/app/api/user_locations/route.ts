a57da22e9183423186108dc09625c9b1a3af9b01
Rick Vermeil <rickv85@gmail.com>
2023-12-14 14:31:27 -0700

    Use class UserLocation to form newUserLoc in POST

--- src/app/api/user_locations/route.ts
+++ src/app/api/user_locations/route.ts
@@ -2,7 +2,7 @@ import { UserLocation } from "@/app/Classes/UserLocation";
 import { sql } from "@vercel/postgres";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET (request: NextRequest) {
+export async function GET(request: NextRequest) {
   try {
     const userId = await request.nextUrl.searchParams.get("user_id");
     // Could provide another searchParam for id number
@@ -10,7 +10,7 @@ export async function GET (request: NextRequest) {
     const userLocations =
       await sql`SELECT * FROM sendtemps.user_locations WHERE user_id = ${userId};`;
     const userLocRows = userLocations.rows;
-    console.log(userLocations)
+    console.log(userLocations);
     const response = NextResponse.json(userLocRows, { status: 200 });
     return response;
   } catch (error) {
@@ -18,18 +18,27 @@ export async function GET (request: NextRequest) {
   }
 }
 
-export async function POST (request: NextRequest) {
+export async function POST(request: NextRequest) {
   try {
-    const newUserLocation = await request.json();
-    console.log(newUserLocation);
+    const reqBody = await request.json();
+    const newUserLoc = new UserLocation(
+      reqBody.name,
+      reqBody.latitude,
+      reqBody.longitude,
+      reqBody.user_id
+    );
+    console.log(newUserLoc);
     let response;
     // Could add a sql query here to see if this has already been created
-    if (newUserLocation) {
-      await sql`INSERT INTO sendtemps.user_locations (name, latitude, longitude, date_created, last_modified) VALUES (${newUserLocation.name}, ${newUserLocation.latitude}, ${newUserLocation.longitude}, ${newUserLocation.date_created}, ${newUserLocation.last_modified})`;
-      response = NextResponse.json(`New location "${newUserLocation.name} created for user: ${newUserLocation.user_id}`, { status: 201 });
+    if (newUserLoc) {
+      await sql`INSERT INTO sendtemps.user_locations (name, latitude, longitude, date_created, last_modified) VALUES (${newUserLoc.name}, ${newUserLoc.latitude}, ${newUserLoc.longitude}, ${newUserLoc.date_created}, ${newUserLoc.last_modified})`;
+      response = NextResponse.json(
+        `New location "${newUserLoc.name} created for user: ${newUserLoc.user_id}`,
+        { status: 201 }
+      );
     }
     return response;
   } catch (error) {
-    return NextResponse.json({ error }, { status: 500 })
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
