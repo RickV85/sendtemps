68e3b55916afcdd3d4501921874e9f3e38d91708
Rick Vermeil <rickv85@gmail.com>
2024-01-02 07:33:39 -0700

    Add next-pwa pkg, update next.config withPWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')
+
 const nextConfig = {
+  ...withPWA({
+    dest: 'public',
+    register: 'true',
+    skipWaiting: 'true',
+  }),
   images: {
     remotePatterns: [
       {
