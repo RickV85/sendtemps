9f6638afbcfa1b568af5a1b37129359f320bd035
Rick Vermeil <rickv85@gmail.com>
2024-01-02 17:03:59 -0700

    Add potential fix for service worker 404

--- next.config.js
+++ next.config.js
@@ -5,6 +5,23 @@ const withPWA = require("next-pwa")({
   register: true,
   // Disables in development
   disable: process.env.NODE_ENV === "development",
+  exclude: [
+    // add buildExcludes here
+    ({ asset, compilation }) => {
+      if (
+        asset.name.startsWith("server/") ||
+        asset.name.match(
+          /^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/
+        )
+      ) {
+        return true;
+      }
+      if (isDev && !asset.name.startsWith("static/runtime/")) {
+        return true;
+      }
+      return false;
+    },
+  ],
 });
 
 const nextConfig = withPWA({
