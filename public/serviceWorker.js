b7e0012fe0fec9d494c596ee6749287767a01a9e
Rick Vermeil <rickv85@gmail.com>
2024-01-06 11:22:27 -0700

    Try new service worker file on live site

---
+++ public/serviceWorker.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", () => {
+  console.log("sw installed")
+})
+
+self.addEventListener("activate", () => {
+  console.log("sw activated")
+})
+
+// Added to trigger browser add to home screen pop-up
+self.addEventListener("fetch", e => {
+  console.log("sw fetch event", e)
+  })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
     if ("serviceWorker" in navigator) {
       window.addEventListener("load", () => {
         navigator.serviceWorker
-          .register("/sw.js")
+          .register("/serviceWorker.js")
           .then((registration) => {
             console.log(
               "Service Worker registered with scope:",
