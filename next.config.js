72682b42b353e774c3de7809596f8a5a4ec24faa
Rick Vermeil <rickv85@gmail.com>
2024-01-06 12:25:39 -0700

    Remove and uninstall Next-PWA, use simple serviceWorker

--- next.config.js
+++ next.config.js
@@ -1,31 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-// const isDev = process.env.NODE_ENV === "development";
-
-// const withPWA = require("next-pwa")({
-//   dest: "public",
-//   register: true,
-//   // Disables in development
-//   disable: isDev,
-//   exclude: [
-//     // add buildExcludes here
-//     ({ asset, compilation }) => {
-//       if (
-//         asset.name.startsWith("server/") ||
-//         asset.name.match(
-//           /^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/
-//         )
-//       ) {
-//         return true;
-//       }
-//       if (isDev && !asset.name.startsWith("static/runtime/")) {
-//         return true;
-//       }
-//       return false;
-//     },
-//   ],
-// });
-
 const nextConfig = {
   images: {
     remotePatterns: [
