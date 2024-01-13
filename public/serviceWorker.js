0682bfb6be09588b60accf52619e1f98bebc00cd
Rick Vermeil <rickv85@gmail.com>
2024-01-13 07:57:11 -0700

    Update fetch listener in serviceWorker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -7,6 +7,13 @@ self.addEventListener("activate", () => {
 });
 
 // Added to trigger browser add to home screen pop-up
-self.addEventListener("fetch", (e) => {
-  // console.log("sw fetch event", e);
+// Changed to allow network requests in standalone
+self.addEventListener('fetch', event => {
+  event.respondWith(
+      fetch(event.request)
+          .catch((err) => {
+              alert(`An error occurred with the network.
+              Please try your request again. ${err}`)
+          })
+  );
 });
