d866a5b80e637b5c892055887a8456c91e8ed17e
Rick Vermeil <rickv85@gmail.com>
2024-02-25 09:48:51 -0700

    Create intercepts for Climbing > Lower Boulder Canyon requests

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -9,6 +9,24 @@ describe("home", () => {
     cy.intercept("http://localhost:3000/api/default_locations", {
       fixture: "default_locs.json",
     });
+
+    // Intercept Climbing - Lower Boulder Canyon fetchNoaaGridLocation call
+    cy.intercept("https://api.weather.gov/points/40.0045,-105.3558", {
+      fixture: "location_details.json",
+    });
+
+    // Intercept Lower Boulder Canyon Detailed daily forecast
+    cy.intercept("https://api.weather.gov/gridpoints/BOU/43,72/forecast", {
+      fixture: "detailed_forecast.json",
+    });
+
+    // Intercept Lower Boulder Canyon hourly forecast
+    cy.intercept(
+      "https://api.weather.gov/gridpoints/BOU/43,72/forecast/hourly",
+      {
+        fixture: "hourly_forecast.json",
+      }
+    );
   });
 
   it("should display the site title, 'SendTemps'", () => {
