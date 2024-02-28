5e2cb13bc768c1b8ca11983bc3f1fd748fe5bf67
Rick Vermeil <rickv85@gmail.com>
2024-02-28 06:51:57 -0700

    Add wait time to HomeAuthed tests

--- cypress/e2e/HomeAuthed.cy.ts
+++ cypress/e2e/HomeAuthed.cy.ts
@@ -12,10 +12,9 @@ describe("initial display for an authorized user", () => {
     );
 
     // Intercept user location req
-    cy.intercept(
-      "/api/user_locations?user_id=101000928729222042760",
-      { fixture: "user_locs.json" }
-    );
+    cy.intercept("/api/user_locations?user_id=101000928729222042760", {
+      fixture: "user_locs.json",
+    });
 
     // Intercept default locations req
     cy.intercept("/api/default_locations", {
@@ -33,16 +32,22 @@ describe("initial display for an authorized user", () => {
   });
 
   it("should show the Edit Locations button to an authorized user", () => {
+    cy.wait(1500);
+
     cy.get("nav.home-nav")
       .find("button#navLocationBtn")
       .should("have.text", "Edit Locations");
   });
 
   it("should not display a Sign In button", () => {
+    cy.wait(1500);
+
     cy.get("button.user-profile-login-button").should("not.exist");
   });
 
   it("should show the user's profile info display", () => {
+    cy.wait(1500);
+
     cy.get("div.user-profile-div").as("userProfile");
     cy.get("@userProfile").contains("Rick Vermeil");
     cy.get("@userProfile")
@@ -52,24 +57,30 @@ describe("initial display for an authorized user", () => {
   });
 
   it("should display the site title, 'SendTemps'", () => {
+    cy.wait(1500);
+
     cy.get("h1").should("have.text", "SendTemps");
   });
 
   it("should display the type-select input and default to 'Select Sport'", () => {
+    cy.wait(1500);
+
     cy.get("select.type-select")
       .find("option:selected")
       .should("have.text", "Select Sport");
   });
 
   it("should display the Welcome Message once loaded", () => {
-    cy.wait(250);
+    cy.wait(1500);
+
     cy.get("section.forecast-section")
       .find("div.home-welcome-msg-div>h2")
       .should("have.text", "Welcome to SendTemps!");
   });
 
   it("should show the proper Welcome Message tailored to authorized user", () => {
-    cy.wait(250);
+    cy.wait(1500);
+
     cy.get("div.home-welcome-msg-div").contains(
       "Click the “Edit Locations” button in the upper left corner to add a new custom location or edit an existing custom location."
     );
