aa48489d4d4b0069be00437860a7c9b4c8a6c44f
Rick Vermeil <rickv85@gmail.com>
2024-03-07 05:59:53 -0700

    Add new icon files to render based on poiType of MapPin

--- src/app/Components/MapPin/MapPin.tsx
+++ src/app/Components/MapPin/MapPin.tsx
@@ -7,11 +7,22 @@ interface Props {
 }
 
 export default function MapPin({title, poiType} : Props) {
+  const iconType = () => {
+    switch (poiType) {
+      case "climb" : return "/climbing-pin.png";
+      case "mtb" : return "/mtb-pin.png";
+      case "ski" : return "/snowflake-pin.png";
+      default : return "/orange-pin.png";
+    }
+  }
+  // <a target="_blank" href="https://icons8.com/icon/10018/winter">Snowflake</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+  // <a target="_blank" href="https://icons8.com/icon/9780/climbing">Climbing</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+  // <a target="_blank" href="https://icons8.com/icon/9823/mountain-biking">Mountain Biking</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
   return (
     <div className={styles.container}>
       <h4 className={styles.title}>{title}</h4>
       <Image
-        src={"/orange-pin.png"}
+        src={iconType()}
         fill={true}
         sizes="5vw"
         priority={false}
