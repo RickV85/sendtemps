f6e89a4a0a72caf85b95ed2fae54b22de7e793c1
Rick Vermeil <rickv85@gmail.com>
2024-04-01 10:23:17 -0600

    Remove serviceWorker.js and install useEffect, was causing issues with Next caching and not needed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,25 +37,6 @@ export default function Home() {
     };
   }, [setPageLoaded]);
 
-  // Register service worker in production
-  useEffect(() => {
-    if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
-      window.addEventListener("load", () => {
-        navigator.serviceWorker
-          .register("/serviceWorker.js")
-          .then((registration) => {
-            console.log(
-              "Service Worker registered with scope:",
-              registration.scope
-            );
-          })
-          .catch((error) => {
-            console.error("Service Worker registration failed:", error);
-          });
-      });
-    }
-  }, []);
-
   // SetScreen width with throttling
   useEffect(() => {
     const setWindowWidthState = throttle(() => {
