f20575f56767385caebd04a7d7abaa27d9d79f82
Rick Vermeil <rickv85@gmail.com>
2024-04-01 10:01:44 -0600

    Disable all SW functions for testing network caching

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,19 +1,19 @@
-self.addEventListener("install", () => {
+// self.addEventListener("install", () => {
   // console.log("sw installed");
-});
+// });
 
-self.addEventListener("activate", () => {
+// self.addEventListener("activate", () => {
   // console.log("sw activated");
-});
+// });
 
 // Added to trigger browser add to home screen pop-up
 // Changed to allow network requests in standalone
-self.addEventListener('fetch', event => {
-  event.respondWith(
-      fetch(event.request)
-          .catch((err) => {
-              alert(`An error occurred with the network.
-              Please try your request again. ${err}`)
-          })
-  );
-});
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//       fetch(event.request)
+//           .catch((err) => {
+//               alert(`An error occurred with the network.
+//               Please try your request again. ${err}`)
+//           })
+//   );
+// });
