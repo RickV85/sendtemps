485b54d8104ab57e98f36c7d4a6535e26de65c3e
Rick Vermeil <rickv85@gmail.com>
2025-12-03 06:44:04 -0700

    add middleware to block users outside of US and Canada

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+const allowedCountryCodes = ['US', 'CA'];
+
+export function middleware(req: NextRequest) {
+  const country = req.geo?.country;
+
+  if (process.env.NODE_ENV === 'production' && (!country || !allowedCountryCodes.includes(country))) {
+    const html = `<!doctype html><html><body><h1>Access Restricted</h1><p>Available only in US & Canada.</p></body></html>`;
+    return new NextResponse(html, {
+      status: 451,
+      headers: { 'content-type': 'text/html' },
+    });
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/:path*'],
+};
